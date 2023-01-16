6e38ea4529f113dc04b254ce9d9c96ab3776e038
Simon Musmos <simonmusmos@gmail.com>
2023-01-17 03:02:37 +0800

    fix: fix table name

--- database/migrations/2023_01_16_185600_add_default_value_for_user_type_id.php
+++ database/migrations/2023_01_16_185600_add_default_value_for_user_type_id.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->integer('user_type_id')->default(1)->change();
         });
     }
