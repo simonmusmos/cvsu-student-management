0520069a26bcb3cc2167c3974a45d74f5a9612e1
Simon Musmos <simonmusmos@gmail.com>
2023-03-06 20:07:40 +0800

    feat: add seat seclection in api

---
+++ app/Http/Controllers/api/SeatController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Section;
+
+class SeatController extends Controller
+{
+    private $status_code = 200;
+    public function getSeats(Request $request) {
+        return 123;
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\SeatController;
 use App\Http\Controllers\api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/login', [UserController::class, 'userLogin']);
 Route::get('/auth/logout', [UserController::class, 'userLogout']);
 Route::get('/auth/details', [UserController::class, 'userDetail'])->middleware('auth_api:sanctum');
+
+Route::get('/section/seats', [SeatController::class, 'getSeats'])->middleware('auth_api:sanctum');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
