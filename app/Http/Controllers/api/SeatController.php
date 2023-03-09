7ecdbbdbd744f71e66158ade307631e7f7b08ae0
Simon Musmos <simonmusmos@gmail.com>
2023-03-09 17:13:22 +0800

    feat: student seats

--- app/Http/Controllers/api/SeatController.php
+++ app/Http/Controllers/api/SeatController.php
@@ -19,7 +19,7 @@ class SeatController extends Controller
     }
 
     public function checkSeats(Request $request) {
-        $exists = UserSeat::where('student_id', $request->user()->student)->whereDate('created_at', Carbon::today())->first();
+        $exists = UserSeat::where('student_id', $request->user()->student->id)->whereDate('created_at', Carbon::today())->first();
         $owner = "";
         if (! $exists) {
             $seat_exists = UserSeat::where('seat', $request->seat)->whereDate('created_at', Carbon::today())->first();
@@ -33,7 +33,7 @@ class SeatController extends Controller
     }
 
     public function useSeat(Request $request) {
-        $exists = UserSeat::where('student_id', $request->user()->student)->whereDate('created_at', Carbon::today())->first();
+        $exists = UserSeat::where('student_id', $request->user()->student->id)->whereDate('created_at', Carbon::today())->first();
         if (! $exists) {
             $seat_exists = UserSeat::where('seat', $request->seat)->whereDate('created_at', Carbon::today())->first();
             if (! $seat_exists) {
