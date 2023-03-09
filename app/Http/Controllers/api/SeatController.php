9b9540d60c9f2ab7294b9f29d11bda2ea05b51f2
Simon Musmos <simonmusmos@gmail.com>
2023-03-09 17:24:00 +0800

    feat: student seats

--- app/Http/Controllers/api/SeatController.php
+++ app/Http/Controllers/api/SeatController.php
@@ -9,13 +9,23 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\UserSeat;
 use App\Models\Student;
 use App\Models\User;
+use App\Models\SectionSeat;
 use Carbon\Carbon;
 
 class SeatController extends Controller
 {
     private $status_code = 200;
+    // public function getSeats(Request $request) {
+    //     return $request->user()->student->section->seats->pluck('seat')->toArray();
+    // }
+
     public function getSeats(Request $request) {
-        return $request->user()->student->section->seats->pluck('seat')->toArray();
+        
+        $students = $request->user()->student->section->students->pluck('id')->toArray();
+        $occupied = UserSeat::whereIn('student_id', $students)->whereDate('created_at', Carbon::today())->get()->pluck('seat')->toArray();
+        $seats = SectionSeat::whereNotIn('seat', $occupied)->where('section_id', $request->user()->student->section_id)->get()->pluck('seat')->toArray();
+        return response()->json(['occupied' => $occupied, 'seats' => $seats]);
+        // return $request->user()->student->section->seats->pluck('seat')->toArray();
     }
 
     public function checkSeats(Request $request) {
