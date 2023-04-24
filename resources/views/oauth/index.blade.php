f0aa8e3908e7bb137c7265a0fa74751b29d828c2
Simon Musmos <simonmusmos@gmail.com>
2023-04-24 14:33:17 +0800

    fix

--- resources/views/oauth/index.blade.php
+++ resources/views/oauth/index.blade.php
@@ -6,5 +6,5 @@
         <h1 class="h3 mb-3 fw-normal">Login</h1>
 
         <a class="w-100 btn btn-lg btn-primary" href="{{ route('oauth.redirect', ['google']) }}" target="_blank">Login using Google</a>
-        <a class="w-100 btn btn-lg btn-danger" href="{{ route('oauth.redirect', ['linked_in']) }}" target="_blank">Login using Linkedin</a>
+        <a class="w-100 btn btn-lg btn-danger" href="{{ route('oauth.redirect', ['linkedin']) }}" target="_blank">Login using Linkedin</a>
 @endsection
