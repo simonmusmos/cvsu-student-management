ede74dbfecad648e7f5eddefb510b23a5b0de14f
Simon Musmos <simonmusmos@gmail.com>
2023-04-24 11:14:40 +0800

    oauth login

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -26,7 +26,7 @@ class SocialiteController extends Controller
      
             $user = Socialite::driver($type)->user();
       
-            dd($user);
+            dd($user->user);
             $linkedinUser = User::where('oauth_id', $user->id)->first();
       
             if($linkedinUser){
