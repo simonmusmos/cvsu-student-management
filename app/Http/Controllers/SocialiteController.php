55cea07ba3aab77f6a700d6e6437ccc7dc9d0522
Simon Musmos <simonmusmos@gmail.com>
2023-05-04 12:12:15 +0800

    test

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -28,7 +28,7 @@ class SocialiteController extends Controller
         // dd(Socialite::driver($type)->user());
         try {
      
-            $user = Socialite::driver($type)->user();
+            $user = Socialite::driver($type)->stateless()->user();
       
             dd($user);
             $linkedinUser = User::where('oauth_id', $user->id)->first();
